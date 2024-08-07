c01536727327ee56210bab8f38a0ad914a6d126d
danielherman2 <eretudaniel009@gmail.com>
2024-08-07 15:03:29 +0300

    firstcommit

---
+++ lib/homescreen.dart
@@ -0,0 +1,21 @@
+// homescreen.dart
+import 'package:flutter/material.dart';import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  const Homescreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Homescreen'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            
+          ),
+        ],
+      ),
+    );
+  }
+}
